81549360f2ccaa121b0a0ab60462737a0ef1e1cb
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-12-08 17:40:01 +0200

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('.button-start');
 
+const stopBtn = document.querySelector('.button-stop');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+});

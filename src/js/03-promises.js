5c59570d71155960e6aa4c6f1fb2e9d621a972a9
Serhii-Fesianov <littlefox06042000@gmail.com>
2023-12-10 17:35:56 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const firstDelay = +form.elements.delay.value;
+  const stepDelay = +form.elements.step.value;
+  const amount = +form.elements.amount.value;
+
+  let delay = firstDelay;
+
+  for (let i = 0; i < amount; i++) {
+    createPromise(i + 1, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += stepDelay;
   }
 }

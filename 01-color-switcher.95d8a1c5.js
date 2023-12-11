6ac60110e0c3dfbdca9ff981eec787ba920e8d1c
Serhii-Fesianov <143389508+Serhii-Fesianov@users.noreply.github.com>
2023-12-11 12:29:34 +0000

    Deploying to gh-pages from @ Serhii-Fesianov/goit-js-hw-09@33532aa7709b6dcb76a3416261c6fd6b375d40de 🚀

---
+++ 01-color-switcher.95d8a1c5.js
@@ -0,0 +1,2 @@
+const t=document.querySelector(".button-start"),e=document.querySelector(".button-stop");let d=null;t.addEventListener("click",(()=>{d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.95d8a1c5.js.map
